55d781e70a52c1e6e46b1ce68f8ae1469f0b2b42
IlyaRupeyks18 <rupeyks@gmail.com>
2025-12-13 12:02:51 +0300

    hhh

--- tests/unit.test.js
+++ tests/unit.test.js
@@ -1,10 +1,12 @@
+import bcrypt from "bcrypt";
 import { describe, expect, it } from "vitest";
 import { verifyPassword } from "../db/userRepository";
 
 describe("unit", () => {
   it("should verify password", async () => {
     const password = "password";
-    const passwordHash = await verifyPassword(password, password);
+    const hash = bcrypt.hashSync(password, 10);
+    const passwordHash = verifyPassword(password, hash);
     expect(passwordHash).toBeTruthy();
   });
 });
