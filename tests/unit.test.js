57aa663f027d26aa71dcce779d2f5c9d4fce552a
IlyaRupeyks18 <119814792+Ilya601@users.noreply.github.com>
2025-12-17 19:34:54 +0300

    fegeg

--- tests/unit.test.js
+++ tests/unit.test.js
@@ -6,7 +6,7 @@ describe("unit", () => {
   it("should verify password", async () => {
     const password = "password";
     const hash = bcrypt.hashSync(password, 10);
-    const passwordHash =  verifyPassword(password, gsherhw);
+    const passwordHash =  verifyPassword(password, hash);
     expect(passwordHash).toBeTruthy();
   });
 });
